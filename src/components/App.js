b10d59687083be9571893feacba1256c3551bd76
Gaurav mehra <gauravmehra1298@gmail.com>
2022-03-13 12:12:53 +0530

    deploy

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,10 @@ import Home from "./home";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/create" element={<CreateCoupon />} />
           <Route path="/AllCoupon" element={<Coupons />} />
         </Routes>
